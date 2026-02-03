93a42b57bd3f53a1be286c2043c1094091d18ab6
Phạm Thanh Huy <huyp7922@gmail.com>
2026-02-03 17:56:37 +0700

    update(prisma) : add_table_permisson_role_and_role_permission

--- src/libs/prisma.ts
+++ src/libs/prisma.ts
@@ -1,10 +1,6 @@
 import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+import 'dotenv/config';
 import { PrismaClient } from '../../generated/prisma/client';
 
-const adapter = new PrismaMariaDb({
-    host: process.env.DB_HOST!,
-    user: process.env.DB_USERNAME!,
-    password: process.env.DB_PASSWORD!,
-    database: process.env.DB_DATABASE!,
-});
+const adapter = new PrismaMariaDb(process.env.DATABASE_URL!);
 export const prisma = new PrismaClient({ adapter });

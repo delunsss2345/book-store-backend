4428ae1f354c3fa6cc9e10ce6dfab7b8353249ad
Phạm Thanh Huy <huyp7922@gmail.com>
2026-03-09 09:57:31 +0700

    refactor(db): lowercase supplier table name

--- prisma/migrations/20260308145954_cost_prise_optional_stock_option/migration.sql
+++ prisma/migrations/20260308145954_cost_prise_optional_stock_option/migration.sql
@@ -6,7 +6,7 @@ ALTER TABLE `book_variants` ADD COLUMN `reserved` INTEGER NULL DEFAULT 0,
 ALTER TABLE `books` ADD COLUMN `supplier_id` BIGINT NULL;
 
 -- CreateTable
-CREATE TABLE `Supplier` (
+CREATE TABLE `supplier` (
     `id` BIGINT NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
@@ -16,4 +16,4 @@ CREATE TABLE `Supplier` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `books` ADD CONSTRAINT `books_supplier_id_fkey` FOREIGN KEY (`supplier_id`) REFERENCES `Supplier`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `books` ADD CONSTRAINT `books_supplier_id_fkey` FOREIGN KEY (`supplier_id`) REFERENCES `supplier`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
